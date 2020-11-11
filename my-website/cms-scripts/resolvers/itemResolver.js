226d9b28212632937403ccddf5c804e28d1b50d8
MichaelB <MichaelB@kentico.com>
2020-11-11 10:28:43 -0500

    Added code block type support

--- my-website/cms-scripts/resolvers/itemResolver.js
+++ my-website/cms-scripts/resolvers/itemResolver.js
@@ -1,11 +1,17 @@
 const resolveItemInRichText = (item) => {
-    if (item.system.type === 'callout'){
-        return `:::${item.type.value[0].name} ${item.title.value} ${item.body.value}:::`
+    switch (item.system.type) {
+        case 'callout':
+            return `:::${item.type.value[0].name} ${item.title.value} ${item.body.value}:::`
+
+        case 'youtube_video':     
+            return `<a href="http://www.youtube.com/watch?v=${item.video_id.value}"><img src="http://img.youtube.com/vi/${item.video_id.value}/0.jpg" alt="${item.video_title.value}"/></a>`
+
+        case 'code_block':
+            return `<code>${item.code.value}</code>`
+        
+        default:
+            return `> Content not available.`
     }
-    if (item.system.type === 'youtube_video') {     
-        return `<a href="http://www.youtube.com/watch?v=${item.video_id.value}"><img src="http://img.youtube.com/vi/${item.video_id.value}/0.jpg" alt="${item.video_title.value}"/></a>`
-    }
-    return `> Content not available.`
 }
 
 exports.resolveItemInRichText = resolveItemInRichText;
